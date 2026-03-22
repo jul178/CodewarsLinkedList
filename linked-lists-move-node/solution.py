acd5dc1b3b454737a4455217bcb7b1927a9cb0a3
Julia Dub <juliadub2008@gmail.com>
2026-03-22 15:55:25 +0200

    Add move_node function which takes the node from the front of the source list and moves it to the front of the destintation list

---
+++ linked-lists-move-node/solution.py
@@ -0,0 +1,17 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+def move_node(source, dest):
+    if source is None:
+        raise TypeError
+
+    node = Node(source.data)
+    node.next = dest
+    return Context(source.next, node)
